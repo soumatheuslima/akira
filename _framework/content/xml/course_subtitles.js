f89eb2d994f42bbc0382beba10bd671638042bf6
wilsondefreitasjr <wilsondefreitasjr92@gmail.com>
2016-08-27 19:59:58 -0300

    Tornei a 'subtitleArray" local

--- _framework/content/xml/course_subtitles.js
+++ _framework/content/xml/course_subtitles.js
@@ -1,8 +1,14 @@
-subtitleArray = new Array();
+var subtitleArray = new Array();
 
 var character = 'locutor_mc'; //Nome do personagem
 
-/*OBS: para aplicar um 'checkpoint' sem legenda, apenas deixe o índice do conteudo da legenda vazio ('');*/
+/* new Array('id da legenda', 'instancia do personagem', 'conteudo da legenda');
+
+- para aplicar um 'checkpoint' sem legenda, apenas deixe o índice do conteudo da legenda vazio ('');
+
+- Se o personagem for inserido dentro de um movie clip, de o nome de instancia a esse mc de 'bolota_mc',
+  caso não seja possível, adicione o nome de instancia do mc antes do nome de instancia do personagem, ex: 'movieclip_mc.locutor_mc';
+*/
 
 subtitleArray.push(
 
