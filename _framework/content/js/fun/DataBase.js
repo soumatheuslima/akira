a970d3e794459e66476acd42da370f3ff5e2d330
wilsondefreitasjr <wilsondefreitasjr92@gmail.com>
2016-08-27 20:39:54 -0300

    Correção no método Init

--- _framework/content/js/fun/DataBase.js
+++ _framework/content/js/fun/DataBase.js
@@ -2,8 +2,8 @@ var DataBase = {
 	maxIndex: 0,
 	Index: [],
 	init: function(arg){
-		DataBase.maxIndex = (arg) ? arg : 4;
 		if(DataBase.Index.length > 0) return
+		DataBase.maxIndex = (arg) ? arg : 4;
 		for(var w = 0; w < DataBase.maxIndex; w++ ) DataBase.Index.push(undefined);
 	},
 	setValue: function(arg1, arg2){	DataBase.Index[arg1] = arg2; },
