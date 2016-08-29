9d187aeda0c12a8538ec4ea814a42979b298ab48
wilsondefreitasjr <wilsondefreitasjr92@gmail.com>
2016-08-29 12:15:13 -0300

    Correção
    Ajustei o método 'getValue'

--- _framework/content/js/fun/DataBase.js
+++ _framework/content/js/fun/DataBase.js
@@ -7,5 +7,5 @@ var DataBase = {
 		for(var w = 0; w < DataBase.maxIndex; w++ ) DataBase.Index.push(undefined);
 	},
 	setValue: function(arg1, arg2){	DataBase.Index[arg1] = arg2; },
-	getValue: function(arg){ return DataBase.Index[arg][0];	}
+	getValue: function(arg){ return DataBase.Index[arg];	}
 }
